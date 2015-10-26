e66782bd8cd8a8d7ddcfdf8b5bee5f75db60d4bd
bogotobogo <k@bogotobogo.com>
2015-10-26 10:29:02 -0700

    initial commit

---
+++ fb.js
@@ -0,0 +1,6 @@
+// facebook app settings
+module.exports = {
+	'appID' : '1693394864209499',
+	'appSecret' : 'b237c8f0847ebdb1c4a472db7d46bbb7',
+	'callbackUrl' : 'http://localhost/login/facebook/callback'
+}
---
+++ models/user.js
@@ -0,0 +1,19 @@
+
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('User',{
+	fb: {
+		id: String,
+		access_token: String,
+		firstName: String,
+		lastName: String,
+		email: String
+	},
+	twitter: {
+		id: String,
+		token: String,
+		username: String,
+		displayName: String,
+		lastStatus: String
+	}
+});
